068961061dae8a56229a5af62412ba4b83b8ce77
Sagar Sehwag <isagarsehwag@gmail.com>
2018-08-11 17:13:51 +0530

    made it better

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: 'mongodb://<sagarsehwag>:<Mlab@0026>@ds119422.mlab.com:19422/vidjot-prod'
+        mongoURI: 'mongodb://sagarsehwag:Mlab@0026@ds119422.mlab.com:19422/vidjot-prod'
     }
 } else {
     module.exports = {
